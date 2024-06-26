ca394a2e02c674029fc4f1631c46477cd45dde55
Дзантемир Дзгоев <ivan23654623@yahoo.com>
2024-06-26 15:17:10 +0300

    Update use_stm8dce.py

--- use_stm8dce.py
+++ use_stm8dce.py
@@ -3,7 +3,8 @@ import shutil
 import sys
 Import("env")
 
-# Sanity check that tool was installed
+
+# Sanity check that tool was installed 
 if shutil.which("stm8dce") is None:
     sys.stderr.write("stm8dce tool not found, install it via https://github.com/CTXz/STM8-DCE")
     env.Exit(-1)
@@ -13,20 +14,28 @@ def optimize_asm(source, target, env):
     temp_out_dir = os.path.join(env.subst("$BUILD_DIR"), "optim_output")
     if not os.path.isdir(temp_out_dir):
         os.mkdir(temp_out_dir)
+    
+    asm_path=""
 
-    env.Execute("stm8dce "  + 
-    env.GetProjectOption("stm8dce_flags", default="")   +
-    (" -v " if int(ARGUMENTS.get("PIOVERBOSE", 0)) else "") +
+    for x in source:
+      tst_asm_pth= os.path.splitext(str(x))[0]+".asm"  
+      if  os.path.isfile(tst_asm_pth):
+        asm_path += ' "' + tst_asm_pth +'"'
+
+    env.Execute(env.VerboseAction("stm8dce "  + 
+    stm8dce_flags  +
+    (" -v " if int(ARGUMENTS.get("PIOVERBOSE", 0)) and  (" "+stm8dce_flags+" ").find(" -v ")<0  else "") +
     " -o " + 
-    '"' + temp_out_dir +  
-    '" "'+
-    '" "'.join( [os.path.splitext(str(x))[0]+".asm"   for x in source])+ '"' )    
+    '"' + temp_out_dir + 
+    '" "' +
+    '" "'.join(map(str,source))+ '"' + asm_path, "STM8DCE CODE OPTIMIZATION"))  
   
     for x in source:
         tmp_path= os.path.join(temp_out_dir,os.path.splitext(os.path.basename(str(x)))[0]+".asm") 
-        env.Execute("$AS -plosg -ff -o " + '"' +str(x) +'" "' +tmp_path+'"' )
-        os.remove(tmp_path)
+        if  os.path.isfile(tmp_path):
+            env.Execute(env.VerboseAction("$AS -plosg -ff -o " + '"' +str(x) +'" "' +tmp_path+'"',"COMPILING "+str(x) ))
+            os.remove(tmp_path)
 
     os.rmdir(temp_out_dir)
 
-env.AddPreAction("$BUILD_DIR/${PROGNAME}.elf", optimize_asm)
+env.AddPreAction("$BUILD_DIR/${PROGNAME}.elf", env.VerboseAction(optimize_asm,"EXTRA SCRIPT"))
